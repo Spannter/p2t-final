6795956c744c1d9a6d7cb48898f80fbbd1ef9a24
Terreassa Spann <spannter@outlook.com>
2025-03-31 21:07:45 -0400

    adding login access for users

--- p2t-capstone/frontend/src/main.tsx
+++ p2t-capstone/frontend/src/main.tsx
@@ -5,11 +5,15 @@ import './global.css'
 
 import {BrowserRouter as Router} from "react-router-dom"
 import AppRoutes from './AppRoutes';
+import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate';
 
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
+      <Auth0ProviderWithNavigate>
+      <AppRoutes />
+      </Auth0ProviderWithNavigate>
       <AppRoutes />
     </Router>
   </StrictMode>
